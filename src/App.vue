d298696dc58721bc24a1237368d6138cc444caf9
Claes Nymand Nilsson <claes89@gmail.com>
2024-02-18 23:14:15 +0100

    Added some styling and a footer

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,36 @@
 </script>
 
 <template>
-  <header>
-    <div>
-      <h1 class="font-[Kurale] text-3xl">Claes Nymand Nilsson</h1>
-      <nav>
+  <div class="flex flex-col h-screen">
+    <header class="border-b border-slate-200">
+      <div class="flex justify-between max-w-7xl px-3 py-2 place-items-center">
         <RouterLink to="/">
-          <Button variant="link">
-            Home
-          </Button>
+          <h1 class="font-[Kurale] text-lg sm:text-2xl">Claes Nymand Nilsson</h1>
         </RouterLink>
-        <RouterLink to="/about">
-          <Button variant="link">
-            About</Button>
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+        <nav>
+          <RouterLink to="/">
+            <Button variant="link">
+              Home
+            </Button>
+          </RouterLink>
+          <RouterLink to="/about">
+            <Button variant="link">
+              About</Button>
+          </RouterLink>
+        </nav>
+      </div>
+    </header>
+    <main class="px-3 py-2 max-w-7xl mb-auto">
+      <RouterView />
+    </main>
+    <footer class="px-3 py-2 border-t border-slate-200">
+      <h2>Hello from footer!</h2>
+    </footer>
+  </div>
 </template>
+
+<style>
+.router-link-active button {
+  text-decoration: underline;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,8 @@
 <template>
   <main>
     <p>The website is under construction :)</p>
+    <p v-for="i in Array(10)">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi esse nesciunt minus minima
+      architecto? Vitae aperiam ab ipsam sint labore, voluptatibus, obcaecati cumque repellendus at vel corrupti, adipisci
+      voluptatem tenetur!</p>
   </main>
 </template>
