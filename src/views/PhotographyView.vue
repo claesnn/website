733f45b7e586024087395008e8a0bf1b21aa0c5c
Claes Nymand Nilsson <claes89@gmail.com>
2024-02-27 21:52:19 +0100

    Add dimensions to first 6 pics

--- src/views/PhotographyView.vue
+++ src/views/PhotographyView.vue
@@ -13,13 +13,9 @@ const photos: string[] = [
 <template>
   <h1 class="font-[Kurale] text-5xl text-center mt-5 mb-10">Photography</h1>
   <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-3">
-    <RouterLink
-      v-for="photo in IMAGECOUNT"
-      :key="photo"
-      :to="`/photography/${photo}`">
-      <img
-        :src="`/images/${photo}.webp`"
-        :loading="photo < 7 ? 'eager' : 'lazy'" />
+    <RouterLink v-for="photo in IMAGECOUNT" :key="photo" :to="`/photography/${photo}`">
+      <img :src="`/images/${photo}.webp`" :width="photo < 7 ? 1280 : ''" :height="photo < 7 ? 1920 : ''"
+        :loading="photo < 4 ? 'eager' : 'lazy'" />
     </RouterLink>
   </div>
 </template>
